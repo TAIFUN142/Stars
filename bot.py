c0309e6fde764a39f2271f57aff6bbc7b58d6f86
TAIFUN142 <oleglabubov@gmail.com>
2026-03-27 19:42:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ POCKET_REFERRAL_LINK = "https://pocket-friends.co/r/cvez0moyv8"
 ADMIN_ID = 8385943123
 
 # КАРТИНКИ ДЛЯ СИГНАЛОВ
-BUY_IMAGE_URL = "https://i.imgur.com/zf8dJ2p.png"
+BUY_IMAGE_URL = ""https://i.pinimg.com/originals/65/09/47/65094754459cfa954d89a68ce89c8b15.png""
 SELL_IMAGE_URL = "https://i.imgur.com/0QK9mJH.png"
 # ===============================
 
